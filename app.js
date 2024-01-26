0da1744144201856f610b6bfb1c5cf198c25fa25
syfrezia <syafrie.mufariza98@gmail.com>
2024-01-26 21:18:21 +0700

    CI: implement cookie session for storing session

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cookieSession = require("cookie-session");
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_KEY],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    secure: false, // set to true if your using https or in production
+    httpOnly: true,
+    domain: "localhost",
+    path: "/",
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
