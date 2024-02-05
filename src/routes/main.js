26bff1cec1b8c3d4f0af8ab044fe231de641ce34
Danazzz <artaputra95@gmail.com>
2024-02-05 17:09:59 +0700

    feat: add all controller in 1 router file

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const { register, login, logout } = require('../controllers/authController');
+const { createInventory, getInventories, getInventoryByProduct, updateInventory, deleteInventory} = require("../controllers/inventoryController");
+const { createProduct, getProducts, getProductById, updateProduct, deleteProduct } = require("../controllers/productController");
+const { addToCart, updateCartItem, deleteCartItem, getTotalPrice, submitOrder } = require("../controllers/cartController");
+const { createTransaction, getTransactions, getTransactionDetails, updateTransaction, deleteTransaction} = require("../controllers/transactionController");
+
 
 const router = express.Router();
 
@@ -7,5 +12,28 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 
+router.post("/", createInventory);
+router.get("/", getInventories);
+router.get("/:product_id", getInventoryByProduct);
+router.put("/:product_id", updateInventory);
+router.delete("/:product_id", deleteInventory);
+
+router.post("/", createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+router.post('/cart/add', addToCart);
+router.put('/cart/update/:cartItemId', updateCartItem);
+router.delete('/cart/delete/:cartItemId', deleteCartItem);
+router.get('/cart/total', getTotalPrice);
+router.post('/cart/submit', submitOrder);
+
+router.post('/transactions', createTransaction);
+router.get('/transactions', getTransactions);
+router.get('/transactions/:transactionCode', getTransactionDetails);
+router.put('/transactions/:transactionCode', updateTransaction);
+router.delete('/transactions/:transactionCode', deleteTransaction);
+
 module.exports = router;
-``
